da96eb04ad92c199849f348652c7c98ac099ef55
fcolic89 <filip.colic00@gmail.com>
2024-03-31 12:15:28 +0200

    (zsh): fixed opening directories with nvim in __open_in_editor

--- zsh/key-bindings.zsh
+++ zsh/key-bindings.zsh
@@ -2,7 +2,7 @@ __open_in_editor(){
   local option=$(command find . -not -path '*/node_modules*' -not -path '*.git*' | command fzf --reverse)
   if [ -n "$option" ]; then
     local option_path=$(command realpath $option || command readlink -f $option)
-    if [ "$EDITOR" = "nvim" ]; then
+    if [ "$EDITOR" = "nvim" ] && [ -f "$option_path" ]; then
       $EDITOR -c "cd $(command dirname $option_path)" $option_path
     else
       $EDITOR $option_path
