8b3e5560f3af102e2d9b34ca1e5b4e17eb6d1734
fcolic89 <filip.colic00@gmail.com>
2024-04-05 22:45:04 +0200

    (docker): functions rework

--- docker/functions.zsh
+++ docker/functions.zsh
@@ -1,57 +1,63 @@
-#stops a specified docker container
-#or all running docker containers if the first argument is -a
+#stops a one or all docker containers
 function dstop(){
-  if [[ $1 = "-a" ]]
-  then
-    docker container stop $(docker ps -q)
-  elif [[ ! -z $1 ]]
-  then
-    docker container stop $1
+  if [ "$1" = "-a" ]; then
+    command docker container stop $(command docker ps -q)
+  elif [ -n "$1" ]; then
+    command docker container stop "$1"
   else
-    echo "Missing an argument!"
+    if [ ! $(command -v "fzf") ]; then
+      echo "Error: fzf needs to be installed to use this function.(https://github.com/junegunn/fzf)"
+      return 1
+    fi
+    local options=$(command docker ps | command awk 'NR > 1 {print $1, $2, $NF}')
+    local selected_option=$(command echo "${options[@]}" | command fzf --header=$'CONTAINER ID  IMAGE  NAME\n' --layout=reverse --prompt="Select a container: ")
+    command docker container stop $(echo $selected_option | command awk '{print $3}')
   fi 
 }
 
-#remove a specified docker image
-#or all images if the first argument is -a
+#remove one or all docker images
 function drmi(){
-  if [[ $1 = "-a" ]]
-  then
+  if [ "$1" = "-a" ]; then
     docker rmi $(docker images -q)
-  elif [[ ! -z $1 ]]
-  then
-    docker rmi $1
+  elif [ -n "$1" ]; then
+    docker rmi "$1"
   else
-    echo "Missing an argument!"
+    if [ ! $(command -v "fzf") ]; then
+      echo "Error: fzf needs to be installed to use this function.(https://github.com/junegunn/fzf)"
+      return 1
+    fi
+    local options=$(command docker images | command awk 'NR > 1 {print $1, $2, $3}')
+    local selected_option=$(command echo "${options[@]}" | command fzf --header=$'REPOSITORY  TAG  IMAGE ID\n' --layout=reverse --prompt="Select an image: ")
+    command docker rmi $(echo $selected_option | command awk '{print $3}')
   fi
 }
 
-#removes a specified stopped docker container
-#or all stopped containers if the first argument is -a
+#removes one or all docker container
 function drm(){
-  if [[ $1 = "-a" ]]
-  then
+  if [ "$1" = "-a" ]; then
     docker rm $(docker ps -aq)
-  elif [[ ! -z $1 ]]
-  then
-    docker rm $1
+  elif [ -n "$1" ]; then
+    docker rm "$1"
   else
-    echo "Missing an argument!"
+    if [ ! $(command -v "fzf") ]; then
+      echo "Error: fzf needs to be installed to use this function.(https://github.com/junegunn/fzf)"
+      return 1
+    fi
+    local options=$(command docker ps -f "status=exited" | command awk 'NR > 1 {print $1, $2, $NF}')
+    local selected_option=$(command echo "${options[@]}" | command fzf --header=$'CONTAINER ID  IMAGE  NAME\n' --layout=reverse --prompt="Select an image: ")
+    command docker rm $(echo $selected_option | command awk '{print $3}')
   fi
 }
 
 function dcu(){
-  if [[ -z $1 ]]
-  then
+  if [ -z "$1" ]; then
     docker compose up 
-  elif [[ $1 = "-p" ]] && [[ ! -z $2 ]]
-  then
-   docker compose --profile $2 up $3
-  elif [[ ! -z $1 ]] && [[ $1 != "-p" ]]
-  then
-   docker compose up $1 $2
+  elif [ "$1" = "-p" ] && [ -n "$2" ]; then
+   docker compose --profile $2 up ${@:3}
+  elif [ -n "$1" ] && [ "$1" != "-p" ]; then
+   docker compose up $@
   else
-    echo "An error occured while trying to call docker compose up!"
+    echo "An error occurred while trying to call docker compose up!"
   fi
 }
 
@@ -62,6 +68,6 @@ function dbash(){
     return 1
   fi
   local options=$(command docker ps | command awk 'NR > 1 {print $1, $2, $NF}')
-  local selected_option=$(command printf "%s\n" "${options[@]}" | command fzf --header=$'CONTAINER ID  IMAGE  NAME\n' --layout=reverse --prompt="Select a container: ")
-  command docker exec -it $(command echo $selected_option | awk '{print $3}') /bin/bash $@
+  local selected_option=$(command echo "${options[@]}" | command fzf --header=$'CONTAINER ID  IMAGE  NAME\n' --layout=reverse --prompt="Select a container: ")
+  command docker exec -it $(command echo $selected_option | command awk '{print $3}') /bin/bash $@
 }
