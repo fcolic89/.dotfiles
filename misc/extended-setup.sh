69e3f150dac72276bc94ef1e707129aa398b8974
fcolic89 <filip.colic00@gmail.com>
2024-10-19 14:54:11 +0200

    (setup,extended-setup): updated scripts

--- misc/extended-setup.sh
+++ misc/extended-setup.sh
@@ -1,14 +1,11 @@
 #!/usr/bin/env bash
 
- #set -e
-
 color_yellow='\e[33m'
-color_red='\e[31m'
 nocolor='\e[0m'
 dotfiles="$HOME/.dotfiles"
 
 info_message() {
-    echo -e "$color_yellow[EXTEDED SETUP]=> $1$nocolor"
+  echo -e "$color_yellow[EXTEDED SETUP]=> $1$nocolor"
 }
 
 info_message "Linking tmux config"
@@ -17,16 +14,12 @@ ln -sv "$dotfiles/tmux/.tmux.conf" "$HOME/.tmux.conf"
 info_message "Linking vim config"
 ln -sv "$dotfiles/vim/.vimrc" "$HOME/.vimrc"
 
-if [ $(echo "$XDG_CURRENT_DESKTOP" | grep "GNOME") ]; then
+if [ $(echo "$XDG_CURRENT_DESKTOP" | grep -q "GNOME") ]; then
   read -p "$(info_message "Setup gnome keybinds [y/N]: ")" awn
-  if [[ "$awn" == [Yy]* ]]; then
-    . $dotfiles/misc/gnome-keybinds.sh
-  fi
+  [[ "$awn" == [Yy]* ]] && source $dotfiles/misc/gnome-keybinds.sh
 fi
 
 if [ $(command -v "git") ]; then
   read -p "$(info_message "Setup git [y/N]: ")" awn
-  if [[ "$awn" == [Yy]* ]]; then
-    . $dotfiles/git/setup-git.sh
-  fi
+  [[ "$awn" == [Yy]* ]] && source $dotfiles/git/setup-git.sh
 fi
