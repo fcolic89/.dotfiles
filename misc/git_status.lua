e7eee0d447e192ab5a81c291d16f5e97588fc808
fcolic89 <filip.colic00@gmail.com>
2026-01-03 15:11:43 +0100

    Lua script fix
    -Fixed how tag and reference name is displayed
    -Fixed the tag name command showing an error when there is no tag referencing current commit
    -Changed method names to use snake_case

--- misc/git_status.lua
+++ misc/git_status.lua
@@ -13,8 +13,8 @@ local git_status = {
   stashed = 0,
 }
 
-function promptPrint()
-  local prompOutput = {
+function prompt_print()
+  local promp_output = {
     branch = "%{$fg_bold[magenta]%}",
     behind = "%{↓%G%}",
     ahead = "%{↑%G%}",
@@ -29,32 +29,32 @@ function promptPrint()
 
   for key, value in pairs(git_status) do
     if value and value ~= 0 then
-      prompOutput[key] = prompOutput[key] .. value .. prompOutput.reset
+      promp_output[key] = promp_output[key] .. value .. promp_output.reset
     else
-      prompOutput[key] = ''
+      promp_output[key] = ''
     end
   end
 
   print(
-    prompOutput.branch ..
-    prompOutput.behind ..
-    prompOutput.ahead ..
-    prompOutput.conflicts ..
-    prompOutput.staged ..
-    prompOutput.changed ..
-    prompOutput.deleted ..
-    prompOutput.untracked ..
-    prompOutput.stashed
+    promp_output.branch ..
+    promp_output.behind ..
+    promp_output.ahead ..
+    promp_output.conflicts ..
+    promp_output.staged ..
+    promp_output.changed ..
+    promp_output.deleted ..
+    promp_output.untracked ..
+    promp_output.stashed
   )
 end
 
-function longPrint()
+function long_print()
   for k, v in pairs(git_status) do
     print(k .. " " .. v)
   end
 end
 
-function shortPrint()
+function short_print()
   print(
     git_status.branch .. " " ..
     git_status.behind .. " " ..
@@ -68,33 +68,37 @@ function shortPrint()
   )
 end
 
-function setStashCount()
+function set_stash_count()
   stash_handle = io.popen('git stash list | grep "$(git rev-parse --abbrev-ref HEAD)" | wc -l')
   git_status.stashed = stash_handle:read("n")
   stash_handle:close()
 end
 
-function getTagOrRef()
-  local tag_handle = io.popen("git describe --tags")
+function get_tag_or_ref()
+  local tag_handle = io.popen("git describe --tags 2> /dev/null")
   local tag = tag_handle:read("*a")
-  tag_handle:close()
+  local tag_ok = tag_handle:close()
 
-  if tag then
-    return tag
-  else
-    local ref_handle = io.popen("git rev-parse --short HEAD")
-    local ref = ref_handle:read("*a")
-    ref_handle:close()
-
-    return ref
+  if tag_ok and tag then
+    return trim(tag)
   end
+
+  local ref_handle = io.popen("git rev-parse --short HEAD")
+  local ref = ref_handle:read("*a")
+  ref_handle:close()
+
+  return trim(ref) .. "..."
 end
 
-function setBranchName(line)
+function trim(s)
+  return s:match "^%s*(.*)":match "(.-)%s*$"
+end
+
+function set_branch_name(line)
   if line:find("Initial commit on") or line:find("No commits yet on") then
     git_status.branch = line:match("(%S+)%s*$")
   elseif line:find("no branch") then
-    git_status.branch = getTagOrRef()
+    git_status.branch = get_tag_or_ref()
   else
     local name_start = 4
     local name_end = line:find("%.%.%.")
@@ -107,7 +111,7 @@ function setBranchName(line)
   end
 end
 
-function setTrackingInfo(line)
+function set_tracking_info(line)
   local _, i = line:find("ahead")
   if i ~= nil then
     git_status.ahead = line:sub(i + 2, i + 2)
@@ -119,7 +123,7 @@ function setTrackingInfo(line)
   end
 end
 
-function getStatus()
+function gather_status_info()
   local status_handle = io.popen("git status --porcelain --branch")
   local status = status_handle:read("*a")
 
@@ -128,8 +132,8 @@ function getStatus()
     local working_tree_status = line:sub(2, 2)
 
     if working_tree_status == "#" and index_status == "#" then
-      setBranchName(line)
-      setTrackingInfo(line)
+      set_branch_name(line)
+      set_tracking_info(line)
     end
 
     if index_status == "A" then
@@ -146,20 +150,20 @@ function getStatus()
   end
 
   status_handle:close()
-  setStashCount()
+  set_stash_count()
 end
 
-getStatus()
+gather_status_info()
 if #arg == 0 then
-  longPrint()
+  long_print()
 end
 
 for _, option in ipairs(arg) do
   if option == "--short" or option == "-s" then
-    shortPrint()
+    short_print()
   elseif option == "--prompt" or option == "-p" then
-    promptPrint()
+    prompt_print()
   else
-    longPrint()
+    long_print()
   end
 end
